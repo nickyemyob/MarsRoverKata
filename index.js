8feeab259314875f8be31ea4fe744380bdbd9cd7
Nick Ye <nick.ye888@gmail.com>
2018-03-02 23:49:42 +1300

    implemented move forward

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+// const moveForward = (location) => {
+//     if(location.direction === 'N'){
+//         return Object.assign({},);
+//     }
+// }
+module.exports = {
+    moveForward: function(location){
+        if(location.direction === 'N'){
+            return {x:location.x, y:location.y+1, direction:location.direction};
+        }
+    }
+}
--- test.spec.js
+++ test.spec.js
@@ -1,11 +1,15 @@
+var rover = require('./index.js');
+
 describe('Rover', () => {
     test('should move forward', () => {
       
       const location = {x:0, y:0, direction:'N'};
-      const actualLocation = moveForward(location);
+      const actualLocation = rover.moveForward(location);
       
-      const expectedLocation = {x:0, y:0, direction:'N'};
+      const expectedLocation = {x:0, y:1, direction:'N'};
 
       expect(actualLocation).toEqual(expectedLocation);
     });
+    
+    
   });
