3c5a1e2e561a6e58daf7bde11538be4a252e8a2a
Nick Ye <nick.ye888@gmail.com>
2018-03-03 17:31:18 +1300

    added move forward when direction is east

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
         if(location.direction === 'N'){
             return {x:location.x, y:location.y+1, direction:location.direction};
         }
+        if(location.direction === 'E'){
+            return {x:location.x+1, y:location.y, direction:location.direction};
+        }
     }
-}
+};
--- test.spec.js
+++ test.spec.js
@@ -1,7 +1,7 @@
 var rover = require('./index.js');
 
 describe('Rover', () => {
-    test('should move forward', () => {
+   test('should move forward when facing north', () => {
       
       const location = {x:0, y:0, direction:'N'};
       const actualLocation = rover.moveForward(location);
@@ -11,5 +11,13 @@ describe('Rover', () => {
       expect(actualLocation).toEqual(expectedLocation);
     });
     
+    test('should move forward when facing east', () => {
+      const location = {x:0, y:0, direction:'E'};
+      const actualLocation = rover.moveForward(location);
+      
+      const expectedLocation = {x:1, y:0, direction:'E'};
+
+      expect(actualLocation).toEqual(expectedLocation);
+    });
     
   });
