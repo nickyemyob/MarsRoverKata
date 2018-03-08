0e79b16b894f11f86d91a78ef664b0d0e93b9976
Nick Ye <nick.ye888@gmail.com>
2018-03-08 23:42:17 +1300

    rover is able to move backwards

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ module.exports = {
         if(location.direction === 'W'){
             return {x:location.x-1, y:location.y, direction:location.direction};
         }
+    },
+
+    moveBackward: function(location){
+        if(location.direction === 'N'){
+            return {x:location.x, y:location.y-1, direction:location.direction};
+        }
+        if(location.direction === 'E'){
+            return {x:location.x-1, y:location.y, direction:location.direction};
+        }
+        if(location.direction === 'S'){
+            return {x:location.x, y:location.y+1, direction:location.direction};
+        }
+        if(location.direction === 'W'){
+            return {x:location.x+1, y:location.y, direction:location.direction};
+        }
     }
 };
--- test.spec.js
+++ test.spec.js
@@ -37,4 +37,41 @@ describe('Rover', () => {
 
       expect(actualLocation).toEqual(expectedLocation);
     });
+
+    test('should move backward when facing north', () => {
+      
+      const location = {x:0, y:0, direction:'N'};
+      const actualLocation = rover.moveBackward(location);
+      
+      const expectedLocation = {x:0, y:-1, direction:'N'};
+
+      expect(actualLocation).toEqual(expectedLocation);
+    });
+    
+    test('should move backward when facing east', () => {
+      const location = {x:0, y:0, direction:'E'};
+      const actualLocation = rover.moveBackward(location);
+      
+      const expectedLocation = {x:-1, y:0, direction:'E'};
+
+      expect(actualLocation).toEqual(expectedLocation);
+    });
+    
+    test('should move backward when facing south', () => {
+      const location = {x:0, y:0, direction:'S'};
+      const actualLocation = rover.moveBackward(location);
+      
+      const expectedLocation = {x:0, y:1, direction:'S'};
+
+      expect(actualLocation).toEqual(expectedLocation);
+    });
+    
+    test('should move backward when facing west', () => {
+      const location = {x:0, y:0, direction:'W'};
+      const actualLocation = rover.moveBackward(location);
+      
+      const expectedLocation = {x:1, y:0, direction:'W'};
+
+      expect(actualLocation).toEqual(expectedLocation);
+    });
   });
