652a9389f9ddb19c52e3738b9116e64db08d3201
Nick Ye <nick.ye888@gmail.com>
2018-03-14 19:44:56 +1300

    refactored moveForward

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const forward = {
+    N: ({x,y,direction}) => ({x, y:y+1, direction}),
+    E: ({x,y,direction}) => ({x:x+1, y, direction}),
+    S: ({x,y,direction}) => ({x, y:y-1, direction}),
+    W: ({x,y,direction}) => ({x:x-1, y, direction}),
+}
+
 module.exports = {
     moveForward: ({x,y,direction}) => {
-        if(direction === 'N') return {x:x, y:y+1, direction:direction};
-        if(direction === 'E') return {x:x+1, y:y, direction:direction};
-        if(direction === 'S') return {x:x, y:y-1, direction:direction};
-        if(direction === 'W') return {x:x-1, y:y, direction:direction};
+        return forward[direction]({x,y,direction});
     },
 
     moveBackward: ({x,y,direction}) => {
@@ -26,4 +30,6 @@ module.exports = {
         if(direction === 'S') return {x:x, y:y, direction:'E'};
         if(direction === 'W') return {x:x, y:y, direction:'S'};
     } 
+    //detectAtEdgeOfWorldHorizontal({gridX, currentX})
+    //detectAtEdgeOfWorldVertical({gridY, currentY})
 };
