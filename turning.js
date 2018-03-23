b5f20debc1a3f1bc1883a774f1aabfc5f123b64d
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2018-03-23 16:22:37 +1300

    refactored turning right

--- turning.js
+++ turning.js
@@ -1,9 +1,6 @@
 module.exports = {
- turnRight: ({x,y,direction}) => {
-        if(direction === 'N') return {x:x, y:y, direction:'E'};
-        if(direction === 'E') return {x:x, y:y, direction:'S'};
-        if(direction === 'S') return {x:x, y:y, direction:'W'};
-        if(direction === 'W') return {x:x, y:y, direction:'N'};
+    turnRight: ({x,y,direction}) => {
+        return right[direction]({x,y,direction});
     },
     
     turnLeft: ({x,y,direction}) => {
@@ -13,3 +10,26 @@ module.exports = {
         if(direction === 'W') return {x:x, y:y, direction:'S'};
     }
 } 
+
+const right = {
+    N: ({x,y,direction}) => (rightFacingNorth(x, y, direction)),
+    E: ({x,y,direction}) => (rightFacingEast(x, y, direction)),
+    S: ({x,y,direction}) => (rightFacingSouth(x, y, direction)),
+    W: ({x,y,direction}) => (rightFacingWest(x, y, direction)),
+};
+
+function rightFacingNorth(x, y, direction) {
+    return { x, y, direction:'E'};
+}
+
+function rightFacingEast(x, y, direction) {
+    return { x, y, direction:'S'};
+}
+
+function rightFacingWest(x, y, direction) {
+    return { x, y, direction:'N'};
+}
+
+function rightFacingSouth(x, y, direction) {
+    return { x, y, direction:'W'};
+}
