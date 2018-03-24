e7badfdce05b7037db26f7e6c873a49041afbe6c
Nick Ye <nick.ye888@gmail.com>
2018-03-24 21:24:04 +1300

    refactored turning left

--- turning.js
+++ turning.js
@@ -4,32 +4,52 @@ module.exports = {
     },
     
     turnLeft: ({x,y,direction}) => {
-        if(direction === 'N') return {x:x, y:y, direction:'W'};
-        if(direction === 'E') return {x:x, y:y, direction:'N'};
-        if(direction === 'S') return {x:x, y:y, direction:'E'};
-        if(direction === 'W') return {x:x, y:y, direction:'S'};
+        return left[direction]({x,y,direction});
     }
 } 
 
 const right = {
-    N: ({x,y,direction}) => (rightFacingNorth(x, y, direction)),
-    E: ({x,y,direction}) => (rightFacingEast(x, y, direction)),
-    S: ({x,y,direction}) => (rightFacingSouth(x, y, direction)),
-    W: ({x,y,direction}) => (rightFacingWest(x, y, direction)),
+    N: ({x,y,direction}) => (turnRightFacingNorth(x, y, direction)),
+    E: ({x,y,direction}) => (turnRightFacingEast(x, y, direction)),
+    S: ({x,y,direction}) => (turnRightFacingSouth(x, y, direction)),
+    W: ({x,y,direction}) => (turnRightFacingWest(x, y, direction)),
 };
 
-function rightFacingNorth(x, y, direction) {
+function turnRightFacingNorth(x, y, direction) {
     return { x, y, direction:'E'};
 }
 
-function rightFacingEast(x, y, direction) {
+function turnRightFacingEast(x, y, direction) {
     return { x, y, direction:'S'};
 }
 
-function rightFacingWest(x, y, direction) {
+function turnRightFacingWest(x, y, direction) {
     return { x, y, direction:'N'};
 }
 
-function rightFacingSouth(x, y, direction) {
+function turnRightFacingSouth(x, y, direction) {
     return { x, y, direction:'W'};
+}
+
+const left = {
+    N: ({x,y,direction}) => (turnLeftFacingNorth(x, y, direction)),
+    E: ({x,y,direction}) => (turnLeftFacingEast(x, y, direction)),
+    S: ({x,y,direction}) => (turnLeftFacingSouth(x, y, direction)),
+    W: ({x,y,direction}) => (turnLeftFacingWest(x, y, direction)),
+};
+
+function turnLeftFacingNorth(x, y, direction) {
+    return { x, y, direction:'W'};
+}
+
+function turnLeftFacingEast(x, y, direction) {
+    return { x, y, direction:'N'};
+}
+
+function turnLeftFacingWest(x, y, direction) {
+    return { x, y, direction:'S'};
+}
+
+function turnLeftFacingSouth(x, y, direction) {
+    return { x, y, direction:'E'};
 }
