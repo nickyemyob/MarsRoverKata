b2a8dc52bf3156e341c530bb6fab71a1f889d0c5
Nick Ye <nick.ye888@gmail.com>
2018-04-25 20:20:49 +1200

    add test to check if rover move around the world correctly

--- rover.spec.js
+++ rover.spec.js
@@ -11,4 +11,15 @@ describe("Rover", () => {
 
 		expect(roverResult).toEqual(expectedRoverResult);
 	});
+
+	test("should be at correct destination from starting point and move around the world without any obstacles", () => {
+
+		const initialPosition = {x:0, y:0, direction:"S"};
+		const commands = ["f","r","f","r","f","r","f"];
+		const roverResult = rover.drive(initialPosition, commands);
+
+		const expectedRoverResult = {x:0, y:0, direction:"E"};
+
+		expect(roverResult).toEqual(expectedRoverResult);
+	});
 });
